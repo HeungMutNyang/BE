c9b2d397da943dcf96bbcab1a965852116f63b23
sumin1012 <ksm2521101@naver.com>
2024-07-29 23:25:55 +0900

    Initial commit

---
+++ src/test/java/heungMutNyang/heungMutNyang_backend/HeungMutNyangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package heungMutNyang.heungMutNyang_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeungMutNyangBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
